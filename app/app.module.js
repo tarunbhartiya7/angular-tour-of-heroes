7bb9245167260305bbf320392483dc8f99496877
yo dude <tarunbhartiya7@gmail.com>
2016-10-16 23:33:08 +0530

    Fourth module completed

--- app/app.module.js
+++ app/app.module.js
@@ -14,13 +14,14 @@ var platform_browser_1 = require('@angular/platform-browser');
 //we need to import the FormsModule package in our Angular module
 var forms_1 = require('@angular/forms');
 var app_component_1 = require('./app.component');
+var hero_detail_component_1 = require('./hero-detail.component');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
             imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
-            declarations: [app_component_1.AppComponent],
+            declarations: [app_component_1.AppComponent, hero_detail_component_1.HeroDetailComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
         __metadata('design:paramtypes', [])
